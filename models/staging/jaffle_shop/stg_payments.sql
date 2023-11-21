c2d7e9f38467beaacb53bcef8bf6ee0c131e40f9
antonio chavez <anchcamp9@gmail.com>
2023-11-21 03:16:57 +0000

    add tests

--- models/staging/jaffle_shop/stg_payments.sql
+++ models/staging/jaffle_shop/stg_payments.sql
@@ -7,5 +7,5 @@ select
     -- amount is stored in cents, convert it to dollars
     amount / 100 as amount,
     created as created_at
-from {{ source('stripe','payments')}}
+from {{ source('stripe','payment')}}
 
